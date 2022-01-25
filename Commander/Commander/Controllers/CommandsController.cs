ca004cdc113626f38505a6c940149f672e9fa644
sindhucj <sindhuja.kasirajan@gmail.com>
2022-01-25 12:13:21 +0530

    Initial

---
+++ Commander/Commander/Controllers/CommandsController.cs
@@ -0,0 +1,35 @@
+﻿using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem); 
+        }
+
+
+    }
+}
---
+++ Commander/Commander/Data/MockCommanderRepo.cs
@@ -0,0 +1,28 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                  new Command() { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle  & Pan" },
+                  new Command() { Id = 1, HowTo = "Cut Bread", Line = "Get Knife", Platform = "Knife & Chopping Board" },
+                  new Command() { Id = 2, HowTo = "Make cup of tea", Line = "Place tea bag in cup", Platform = "Kettle  & Cup" }
+        };
+            return commands;
+
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command() { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle  & Pan" };
+        }
+    }
+}
